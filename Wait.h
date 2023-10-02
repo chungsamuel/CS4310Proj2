190fce2488ef211bd448a4a59b592f379bf7868d
Ashlant <95947089+Ashlant@users.noreply.github.com>
2023-10-01 18:34:23 -0700

    Add files via upload
    added wait.h

---
+++ Wait.h
@@ -0,0 +1,17 @@
+#ifndef BIN_SLEEP_SLEEP_H
+#define BIN_SLEEP_SLEEP_H
+
+#include <POSIXApplication.h>
+  
+class Wait : public POSIXApplication
+{
+  public:
+
+Wait(int argc, char **argv);
+
+  virtual ~Wait();
+
+virtual Result exec();
+};
+
+#endif 
